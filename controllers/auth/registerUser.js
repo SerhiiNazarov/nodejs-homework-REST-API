5d0b76e3730f56418d035d75f21a44fdc178007f
SerhiiNazarov <algazira89@gmail.com>
2023-12-22 11:18:18 +0200

    delete database name

--- controllers/auth/registerUser.js
+++ controllers/auth/registerUser.js
@@ -28,7 +28,7 @@ const register = async (req, res) => {
     verificationToken,
   });
 
-  const token = jwt.sign(SECRET_KEY, { expiresIn: "23h" });
+  const token = jwt.sign(SECRET_KEY, { expiresIn: "22h" });
 
   await User.findByIdAndUpdate(user._id, { token });
 
