106e293a864bcc2f030549bf23e056661d568f2c
SerhiiNazarov <algazira89@gmail.com>
2023-12-25 13:03:55 +0200

    token in login

--- controllers/auth/loginUser.js
+++ controllers/auth/loginUser.js
@@ -1,8 +1,11 @@
 const { User } = require("../../models/user");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const { HttpError, ctrlWrapper } = require("../../helpers");
+
+const { SECRET_KEY } = process.env;
 
 const login = async (req, res) => {
   const { email, password } = req.body;
@@ -19,7 +22,16 @@ const login = async (req, res) => {
     throw HttpError(401, "Email or password is wrong");
   }
 
+  const payload = {
+    id: user._id,
+  };
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+
+  await User.findByIdAndUpdate(user._id, { token });
+
   res.status(200).json({
+    token,
     user: {
       name,
       email,
